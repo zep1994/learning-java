c6e1b211a98441aa1aadfd56842f866e3c608d5c
Thomas Matlock <thomasmatlockbba@gmail.com>
2023-01-31 14:42:45 -0600

    run function

--- learning-java/src/Main.java
+++ learning-java/src/Main.java
@@ -1,42 +1,48 @@
-import java.util.Scanner;
-
 public class Main {
+
+    public static double salaryCalculator(int employeeHours, double employeeWage) {
+        double weeklySalary = employeeHours * employeeWage;
+        return weeklySalary * 52;
+    }
     public static void main(String[] args) {
 
-        int employeeAge = 34;
-        double employeeID = 3.34;
-        boolean isActive = true;
-        String employeeFirstName = "Thomas";
-        String employeeLastName = "Matlock";
-        char employeeFirstInitial = employeeFirstName.charAt(0);
-        char employeeLastInitial = employeeLastName.charAt(0);
+        double salary = salaryCalculator(40, 7.25);
+        System.out.println(salary);
 
-        System.out.println(employeeFirstName + " " + employeeLastName + " is Active? " + isActive);
-        Scanner input = new Scanner(System.in);
-        employeeID = input.nextDouble();
-        if (employeeID > 5) {
-            System.out.println("Please Try Again");
-        } else if (employeeID < 5){
-            employeeID = input.nextDouble();
-        } else {
-            System.out.println("I do not understand");
-        }
-
-        while (isActive) {
-            System.out.println("Is Active");
-            String userInput = input.next();
-
-            if(userInput.equals("no")) {
-                isActive = false;
-            }
-        }
-
-        System.out.println(employeeAge);
-        System.out.println(employeeID);
-        System.out.println(employeeFirstInitial);
-        System.out.println(employeeLastInitial);
-        System.out.println(isActive);
-        System.out.println(employeeFirstName);
-        System.out.println(employeeLastName);
+//        int employeeAge = 34;
+//        double employeeID = 3.34;
+//        boolean isActive = true;
+//        String employeeFirstName = "Thomas";
+//        String employeeLastName = "Matlock";
+//        char employeeFirstInitial = employeeFirstName.charAt(0);
+//        char employeeLastInitial = employeeLastName.charAt(0);
+//
+//        System.out.println(employeeFirstName + " " + employeeLastName + " is Active? " + isActive);
+//        Scanner input = new Scanner(System.in);
+//        employeeID = input.nextDouble();
+//        if (employeeID > 5) {
+//            System.out.println("Please Try Again");
+//        } else if (employeeID < 5){
+//            employeeID = input.nextDouble();
+//        } else {
+//            System.out.println("I do not understand");
+//        }
+//
+//        while (isActive) {
+//            System.out.println("Is Active");
+//            String userInput = input.next();
+//
+//            if(userInput.equals("no")) {
+//                isActive = false;
+//            }
+//        }
+//
+//        System.out.println(employeeAge);
+//        System.out.println(employeeID);
+//        System.out.println(employeeFirstInitial);
+//        System.out.println(employeeLastInitial);
+//        System.out.println(isActive);
+//        System.out.println(employeeFirstName);
+//        System.out.println(employeeLastName);
     }
 }
