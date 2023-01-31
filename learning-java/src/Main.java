ec8d000053f2a8906a79a04138124fb016a5b5fd
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-01-31 11:32:36 -0600

    Add Employee Class Info

---
+++ learning-java/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int employeeAge = 34;
+        double employeeID = 3.34;
+        char employeeFirstInitial = 'T';
+        char employeeLastInitial = 'M';
+        boolean isActive = true;
+        String employeeFirstName = "Thomas";
+        String employeeLastName = "Matlock";
+
+        System.out.println(employeeAge);
+        System.out.println(employeeID);
+        System.out.println(employeeFirstInitial);
+        System.out.println(employeeLastInitial);
+        System.out.println(isActive);
+        System.out.println(employeeFirstName);
+        System.out.println(employeeLastName);
+    }
+}
